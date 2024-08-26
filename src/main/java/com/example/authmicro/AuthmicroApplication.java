8f792c8f54d3a6ede7f9095cbd9c19cbe34f95c2
akashwani2209 <akashwani2209@gmail.com>
2024-08-26 21:15:54 +0530

    first commit

---
+++ src/main/java/com/example/authmicro/AuthmicroApplication.java
@@ -0,0 +1,13 @@
+package com.example.authmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthmicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthmicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authmicro/AuthmicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthmicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
